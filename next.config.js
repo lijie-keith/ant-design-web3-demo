9d441d2d2e71909a367e8df7a430dd06fc744e24
lijie <ljkeith@163.com>
2025-04-30 22:17:18 +0800

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["@ant-design", "antd", "rc-util", "rc-pagination", "rc-picker", "rc-input"],
+  experimental: {
+    // 如果 Next.js 有相关实验性配置
+    prefetchInRender: true,
+  },
 }
 
 module.exports = nextConfig
